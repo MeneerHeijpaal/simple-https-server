4a9dd14f9c2c159119b94b06d8081219b47e4ba2
MarkHeijblok <40831608+MeneerHeijpaal@users.noreply.github.com>
2025-01-14 15:25:46 +0100

    Update simple_https_server.py
    Updated the script to be used with Python 3.12.

--- simple_https_server.py
+++ simple_https_server.py
@@ -8,7 +8,7 @@ If this server is NOT trusted, most browsers and apps will not even connect.
 Usage:
     ./server.py [<port>] - Defaults to port 443
 """
-                                                                                                                
+
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
 import ssl
@@ -21,18 +21,14 @@ class S(BaseHTTPRequestHandler):
 
     def do_GET(self):
         self._set_response()
-        # Give reply for debugging
-        # self.wfile.write("GET request for {}".format(self.path).encode('utf-8'))
         logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
         print("#########################\n\n")
 
     def do_POST(self):
-        content_length = int(self.headers.get('content-length', 0)) # <--- Gets the size of data
-        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        content_length = int(self.headers.get('content-length', 0))
+        post_data = self.rfile.read(content_length)
 
         self._set_response()
-        # Give reply for debugging
-        # self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
         logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
                 str(self.path), str(self.headers), post_data.decode('utf-8'))
         print("#########################\n\n")
@@ -40,19 +36,23 @@ class S(BaseHTTPRequestHandler):
 def run(server_class=HTTPServer, handler_class=S, port=443):
     logging.basicConfig(level=logging.INFO)
     server_address = ('', port)
+    
+    # Create SSL context
+    ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+    ssl_context.load_cert_chain(certfile="server.pem", keyfile="key.pem")
+    
     httpd = server_class(server_address, handler_class)
-    logging.info('Starting server...\n')
+    
     try:
-        httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile="server.pem",
-                               keyfile="key.pem",
-                               ssl_version=ssl.PROTOCOL_TLS)
+        # Wrap socket with SSL context
+        httpd.socket = ssl_context.wrap_socket(httpd.socket,
+                                               server_side=True)
         httpd.serve_forever()
     except KeyboardInterrupt:
         pass
-    httpd.server_close()
-    logging.info('Stopping server...\n')
+    finally:
+        httpd.server_close()
+        logging.info('Stopping server...\n')
 
 if __name__ == '__main__':
     from sys import argv
